d2adb063a7a2380c76a0bfbe2f6341bcfe0efb04
GayathriSrividya <gayathrirajavarapu7@gmail.com>
2024-12-09 15:08:30 +0530

    github actions

--- superset/report_api.py
+++ superset/report_api.py
@@ -587,6 +587,7 @@ class ReportAPI(BaseSupersetView):
 
     def publish_report_portal(self, chart):
         published_report_id = chart.hawkeye_report.published_report_id
+        print(json.dumps(chart.hawkeye_report))
         print(chart.hawkeye_report.published_report_id, "hawkeye Published Report ID")
         print(published_report_id, "Published Report ID")
         if published_report_id is None or published_report_id is "":
